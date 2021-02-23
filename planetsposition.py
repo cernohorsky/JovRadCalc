615780107451f28a0dfd92da46640875fb17f142
riske <septonex@gmail.com>
2021-02-23 15:50:41 +0100

    Added Jupiter visibility plot.

--- planetsposition.py
+++ planetsposition.py
@@ -1,20 +1,48 @@
+
 from astropy.time import Time
-from astropy.coordinates import solar_system_ephemeris, EarthLocation
-from astropy.coordinates import get_body, get_moon
-from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from astropy.coordinates import solar_system_ephemeris
+from astropy.coordinates import get_body
+from astropy.coordinates import EarthLocation, AltAz
 from astropy import units as u
-import time;
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+jupal=[]
+jupaz=[]
+time=[]
 
-t = Time("2019-08-11 11:00", scale="utc")
-loc = EarthLocation(lat=38.2464000*u.deg, lon=274.236400*u.deg, height=0*u.m)
+t = Time("2021-02-22 07:30", scale="utc")
+loc = EarthLocation(lat=50.923901*u.deg, lon=8.008528*u.deg, height=300*u.m)
 
-with solar_system_ephemeris.set('jpl'):
-  moon = get_body('moon', t, loc)
+#with solar_system_ephemeris.set('jpl'):
+for hour in range(0, 23):
+    for minute in range(0, 10, 59):
+        timestr="2021-02-22 "+str(hour)+":"+str(minute)
+        print(timestr)
+        t = Time(timestr, scale="utc")
 
-altazframe = AltAz(obstime=t, location=loc, pressure=0)
-moonaz=moon.transform_to(altazframe)
+        with solar_system_ephemeris.set('builtin'):
+            jupiter = get_body('jupiter', t, loc)
 
-print(moonaz.alt.degree,moonaz.az.degree)
+        altazframe = AltAz(obstime=t, location=loc, pressure=0)
+        jupiter_position=jupiter.transform_to(altazframe)
 
-#https://astronomy.stackexchange.com/questions/33003/planets-positions-azimuth-altitude-api
+        #print(jupiter_position.alt.degree, jupiter_position.az.degree)
+        if jupiter_position.alt.degree > 0:
+            jupal.append(jupiter_position.alt.degree)
+            jupaz.append(jupiter_position.az.degree)
+            time.append(timestr)
+# Data for plotting
+#fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+fig, ax = plt.subplots()
 
+#ax.plot(jupaz, jupal)
+ax.plot(time,jupal)
+
+#ax.set_yticks(np.arange(0,91,15))
+
+ax.set(xlabel='az', ylabel='alt',
+       title='Jupiter')
+ax.grid()
+
+plt.show()
