1f86ebb25e41eb056950b391f0f9563b9ffb76d2
Richard Cernohorsky <septonex@gmail.com>
2021-02-23 23:53:48 +0100

    Result only if Jupiter is visible.

--- JovRadEmi.py
+++ JovRadEmi.py
@@ -2,50 +2,26 @@
 
 import math
 import datetime
+from astropy.time import Time
+from astropy.coordinates import solar_system_ephemeris
+from astropy.coordinates import get_body
+from astropy.coordinates import EarthLocation, AltAz
+from astropy import units as u
 
-print('Program to flag Jovian Decametric windows')
-month = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
-week = 42.46360
-pi = 3.141593
-kr = pi / 180
-num1 = open('jovrad.csv', 'w')
-#yy = int(input(("Year for which predictions are required ")))
-yy=2021
-e = math.trunc(((yy-1) / 100))
-print(e)
-f = 2 - e + math.trunc(e/4)
-print(f)
-jd = math.trunc(365.25 * (yy - 1)) + 1721423 + f + .5
-print(jd)
-d0 = jd - 2435108
-print(d0)
-incr = 0
-dmax = 0
-tx = 0
-ty = 0
-yyly = 0
-yylc = 0
-if yy / 400 - math.trunc((yy / 400)) == 0:
-    incr = 1
-    print("in if-1")
-yyly = yy / 4 - math.trunc((yy / 4))
-yylc = yy / 100 - math.trunc((yy / 100))
-if yyly == 0 and yylc != 0:
-    print("in if-2")
-    incr = 1
-ty = 59 + incr
-dmax = 365 + incr
-tx = ty + .5
-num1.write("**************************************************\n")
-pout =     "JOVIAN IO-DECAMETRIC EMISSION PREDICTIONS FOR "  + str(yy) + "\n"
-num1.write(pout)
-num1.write("**************************************************\n")
-num1.write("\n")
-num1.write("Day   Date   Hr(UT)  Io_Phase   CML   Dist(AU)  Source")
-num1.write("\n")
-th = 0
+def jupalt(t):
+    #t = Time("2021-02-22 07:30", scale="utc")
+    t = Time(t, scale="utc")
+    loc = EarthLocation(lat=50.923901*u.deg, lon=8.008528*u.deg, height=300*u.m)
+    with solar_system_ephemeris.set('builtin'):
+        jupiter = get_body('jupiter', t, loc)
 
-while int(th / 24) + 1 <= dmax:
+    altazframe = AltAz(obstime=t, location=loc, pressure=0)
+    jupiter_position=jupiter.transform_to(altazframe)
+
+    return jupiter_position.alt.degree
+
+def compute(elmin):
+    th=elmin/60
     d = d0 + (th / 24)
     v = (157.0456 + .0011159 * d) % 360
     m = (357.2148 + .9856003 * d) % 360
@@ -68,6 +44,7 @@ while int(th / 24) + 1 <= dmax:
     z = (2 * (U1 - U2)) % 360
     U1 = U1 + .472 * math.sin(kr * z)
     U1 = (U1 + 180) % 360
+
     s = ""
     if L3 < 255 and L3 > 200 and U1 < 250 and U1 > 220:
         s = "Io-A"
@@ -75,8 +52,53 @@ while int(th / 24) + 1 <= dmax:
         s = "Io-B"
     if L3 < 350 and L3 > 300 and U1 < 250 and U1 > 230:
         s = "Io-C"
+    return [dt,s];
+
+
+print('Program to flag Jovian Decametric windows')
+month = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
+week = 42.46360
+pi = 3.141593
+kr = pi / 180
+num1 = open('jovrad.csv', 'w')
+#yy = int(input(("Year for which predictions are required ")))
+yy=2021
+e = math.trunc(((yy-1) / 100))
+f = 2 - e + math.trunc(e/4)
+jd = math.trunc(365.25 * (yy - 1)) + 1721423 + f + .5
+d0 = jd - 2435108
+incr = 0
+dmax = 0
+tx = 0
+ty = 0
+yyly = 0
+yylc = 0
+if yy / 400 - math.trunc((yy / 400)) == 0:
+    incr = 1
+    print("in if-1")
+yyly = yy / 4 - math.trunc((yy / 4))
+yylc = yy / 100 - math.trunc((yy / 100))
+if yyly == 0 and yylc != 0:
+    print("in if-2")
+    incr = 1
+ty = 59 + incr
+dmax = 365 + incr
+tx = ty + .5
+num1.write("**************************************************\n")
+pout =     "JOVIAN IO-DECAMETRIC EMISSION PREDICTIONS FOR "  + str(yy) + "\n"
+num1.write(pout)
+num1.write("**************************************************\n")
+num1.write("\n")
+num1.write("Date       Begin End   Dist(AU) Source")
+num1.write("\n")
+elmin = 0
+
+while int(elmin / 60 / 24) + 1 <= dmax:
+    compute_output=compute(elmin)
+    s=compute_output[1]
     if s != "":
-        dy = math.trunc((th / 24)) + 1
+        dy = math.trunc((elmin / 60 / 24)) + 1
+        '''
         h = th - (dy - 1) * 24
         if(dy > th):
             m = math.trunc((dy - tx) / 30.6) + 3
@@ -86,11 +108,24 @@ while int(th / 24) + 1 <= dmax:
             da = dy - (m - 1) * 31
         mn = month[(m-1)]
         #mn = month[(m-1)*3+1:(m-1)*3-1+3]
-        date = datetime.datetime(2021,1,1)+datetime.timedelta(hours=th)
+        '''
+        emi_start = datetime.datetime(2021,1,1)+datetime.timedelta(minutes=elmin)
         #outstring = "%i  %s  %i  %5.3f  %5.3f  %5.3f  %5.3f  %s\n" % (dy, mn, da, h, U1, L3, dt, s)
-        outstring = "%i  %s  %5.3f  %5.3f  %5.3f  %5.3f  %s\n" % (dy, date, h, U1, L3, dt, s)
-        num1.write(outstring)
-    th = th + .5
+        #outstring = "%i  %s  %5.3f  %5.3f  %5.3f  %s\n" % (dy, date, U1, L3, dt, s)
+        x=s
+        while x!= "":
+            x=compute(elmin)
+            x=x[1]
+            elmin = elmin + 1
+
+        emi_stop = datetime.datetime(2021,1,1)+datetime.timedelta(minutes=elmin)
+        if jupalt(str(emi_start))>10 or jupalt(str(emi_stop))>10:
+            print(emi_stop-emi_start)
+            outstring = "%s %s %5.3f %s\n" % (str(emi_start)[0:16], str(emi_stop)[11:16], compute_output[0], "   "+s)
+            num1.write(outstring)
+            print(outstring)
+
+    elmin = elmin + 1
 
 num1.close()
 print("Program Complete  - results in file jovrad.csv")
